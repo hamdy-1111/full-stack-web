32a9fea7eeca2c2351b40c60aaece23381372c1d
Ammar64 <ammar_ramadan151515@hotmail.com>
2024-04-05 15:22:54 +0200

    Added some function to communicate with backend

---
+++ api/login_api.ts
@@ -0,0 +1,30 @@
+interface UserLoginData {
+    username: string,
+    password: string
+};
+
+interface UserRegisterData {
+    useremail: string,
+    username: string,
+    password: string
+};
+
+export async function send_login_data (login_data: UserLoginData) {
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+            Accept: "application/json"
+        },
+        body: JSON.stringify(login_data)
+    })
+}
+
+export async function send_register_data (login_data: UserLoginData) {
+    const response = await fetch("/register", {
+        method: "POST",
+        headers: {
+            Accept: "application/json"
+        },
+        body: JSON.stringify(login_data)
+    })
+}
