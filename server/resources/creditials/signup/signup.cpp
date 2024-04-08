1d5f27af27f52d332edf9b4d719d63afd5151a57
Ammar64 <ammar_ramadan151515@hotmail.com>
2024-04-09 01:05:56 +0200

    Fixed problems with photo, only one username can be taken.

--- server/resources/creditials/signup/signup.cpp
+++ server/resources/creditials/signup/signup.cpp
@@ -3,20 +3,15 @@
 #include "util/security.hpp"
 #include <fstream>
 shared_ptr<http_response> signup_resource::render_POST(const http_request &req) {
+
     string uuid = generate_uuid_v4();
-    for (auto &file_key : req.get_files()) {
-        if (file_key.first == "photo") {
-            for (auto &file : file_key.second) {
-                if( file.second.get_file_size() > 10 * 1024 * 1024 ) {
-                    return shared_ptr<http_response>(new string_response("Photo too large max size 10MB"));
-                }
-                string filename = file.second.get_file_system_file_name();
-                std::ofstream("database/photos/" + uuid + ".png") << std::ifstream(filename).rdbuf();
-            }
-        }
-    }
     string email = req.get_arg("email");
     string username = req.get_arg("username");
+    
+    if( username_exists(username) ) {
+        return shared_ptr<http_response>( new string_response("User exists aborting") );
+    }
+
     string password = req.get_arg("password");
 
     if( email.empty() || username.empty() || password.empty() ) {
@@ -26,6 +21,13 @@ shared_ptr<http_response> signup_resource::render_POST(const http_request &req) 
     string salt = random_string(16);
     string salted_password = salt + password;
     string hashed_password = sha256_string(salted_password);
+
+
+    string photo = string(req.get_arg("photo"));
+    if( photo.size() > 10 * 1024 * 1024 ) { // Photo should be less than 10 MB
+        return shared_ptr<http_response>(new string_response("Maximum size of the photo is 10 MB upload smaller pic"));
+    }
+    std::ofstream("database/photos/" + uuid + ".png") << photo;
 
     
 
@@ -43,5 +45,22 @@ shared_ptr<http_response> signup_resource::render_POST(const http_request &req) 
     } catch (SQLite::Exception &e) {
         std::cout << e.what() << std::endl;
         return shared_ptr<http_response>(new string_response("Something went wrong"));
+    }
+
+
+
+}
+
+bool signup_resource::username_exists(string username) {
+    try {
+        SQLite::Statement query(*DataBaseManager::users ,"SELECT username FROM users WHERE username = ?");
+        query.bind(1, username);
+        while( query.executeStep() ) {
+            return true;
+        }
+        return false;
+    } catch (SQLite::Exception &e) {
+        std::cout << e.what() << std::endl;
+        throw e;
     }
 }
