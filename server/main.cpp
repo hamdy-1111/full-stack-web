00eb2d15e4ca1bb62db4e130932a65182501e9c7
Ammar64 <ammar_ramadan151515@hotmail.com>
2024-04-14 23:10:53 +0200

    removed https for windows

--- server/main.cpp
+++ server/main.cpp
@@ -19,15 +19,12 @@ class w : public webserver {
 int main(int argc, char const *argv[])
 {
 
-    webserver ws = create_webserver(3000)
-        .use_ssl()
-        .https_mem_key("cert/site.key")
-        .https_mem_cert("cert/site.crt")
+    webserver ws = create_webserver(443)
         .file_upload_target(FILE_UPLOAD_MEMORY_ONLY)
         .file_upload_dir("database/cache")
         .generate_random_filename_on_upload()
         .tcp_nodelay();
-
+        
     root_resource root_rc;
     login_resource login_rc;
     signup_resource signup_rc;
@@ -40,7 +37,7 @@ int main(int argc, char const *argv[])
     ws.register_resource("/sign-up", &signup_rc);
     ws.register_resource("/verify-otp", &verify_rc);
 
-    std::cout << "Connect to https://localhost:3000" << std::endl; 
+    std::cout << "Connect to https://localhost:443" << std::endl; 
     ws.start(true);
 
     DataBaseManager::FinalDatabases(); // clean up memory
